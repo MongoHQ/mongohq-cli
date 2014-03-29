6a8a5a16284535078a91105e20c674dac96f3561
Chris Winslett <chris@mongohq.com>
2014-03-29 12:26:45 -0700

    Dried argument validation and added better discriptions in help

--- mongohq.go
+++ mongohq.go
@@ -8,6 +8,25 @@ import (
   "github.com/MongoHQ/controllers"  // MongoHQ CLI functions
 )
 
+func requireArguments(command string, c *cli.Context, argumentsSlice []string, errorMessages []string) {
+  err := false
+
+  for _, argument := range argumentsSlice {
+    if !c.IsSet(argument) {
+      err = true
+      fmt.Println("--" + argument + " is required")
+    }
+  }
+
+  if err {
+    fmt.Println("\nMissing arguments, for more information, run: mongohq " + command + " --help\n")
+    for _, errorMessage := range errorMessages {
+      fmt.Println(errorMessage)
+    }
+    os.Exit(1)
+  }
+}
+
 func main() {
   app := cli.NewApp()
   app.Name = "mongohq"
@@ -26,19 +45,10 @@ func main() {
       Name:      "databases:info",
       Usage:     "information on database",
       Flags:     []cli.Flag {
-        cli.StringFlag { "database,db", "", "<database name>"},
+        cli.StringFlag { "database,db", "<string>", "Database name for more information"},
       },
       Action: func(c *cli.Context) {
-        err := false
-        if c.String("database") == "" {
-          err = true
-          fmt.Println("--database is required")
-        }
-
-        if err {
-          os.Exit(1)
-        }
-
+        requireArguments("databases:info", c, []string{"database"}, []string{})
         controllers.Database(c.String("database"))
       },
     },
@@ -53,26 +63,11 @@ func main() {
       Name:      "deployments:create",
       Usage:     "create a new Elastic Deployment",
       Flags:     []cli.Flag {
-        cli.StringFlag { "database,db", "", "<database-name>"},
-        cli.StringFlag { "region,r", "", "[for a list of regions, run 'mongohq regions']"},
+        cli.StringFlag { "database,db", "<string>", "New database name to be created on your new deployment"},
+        cli.StringFlag { "region,r", "<string>", "Region for deployment. For a list of regions, run 'mongohq regions'"},
       },
       Action: func(c *cli.Context) {
-        err := false
-        if c.String("database") == "" {
-          err = true
-          fmt.Println("--database is required")
-        }
-        if c.String("region") == "" {
-          err = true
-          fmt.Println("--region is required")
-        }
-
-        if err {
-          fmt.Println("")
-          fmt.Println("For more information, run deployments:create --help")
-          os.Exit(1)
-        }
-
+        requireArguments("deployments:create", c, []string{"database", "region"}, []string{})
         controllers.CreateDeployment(c.String("database"), c.String("region"))
       },
     },
@@ -80,19 +75,10 @@ func main() {
       Name:      "deployments:info",
       Usage:     "information on deployment",
       Flags:     []cli.Flag {
-        cli.StringFlag { "deployment,dep", "", "<deployment id>"},
+        cli.StringFlag { "deployment,dep", "<bson_id>", "The id for the deployment for more information"},
       },
       Action: func(c *cli.Context) {
-        err := false
-        if c.String("deployment") == "" {
-          err = true
-          fmt.Println("--deployment is required")
-        }
-
-        if err {
-          os.Exit(1)
-        }
-
+        requireArguments("deployments:info", c, []string{"deployment"}, []string{})
         controllers.Deployment(c.String("deployment"))
       },
     },
@@ -100,21 +86,21 @@ func main() {
       Name:      "deployments:mongostat",
       Usage:     "realtime mongostat",
       Flags:     []cli.Flag {
-        cli.StringFlag{"deployment,dep", "<bson_id>", "deployment id"},
+        cli.StringFlag{"deployment,dep", "<bson_id>", "The id for the deployment for tailing mongostats"},
       },
       Action: func(c *cli.Context) {
-        if c.String("deployment") != "<bson_id>" {
-          controllers.DeploymentMongoStat(c.String("deployment"))
-        } else {
-          fmt.Println("--deployment is required")
-          os.Exit(1)
-        }
+        requireArguments("deployments:mongostat", c, []string{"deployment"}, []string{})
+        controllers.DeploymentMongoStat(c.String("deployment"))
       },
     },
     {
       Name:      "deployments:logs (pending)",
       Usage:     "tail logs",
+      Flags:     []cli.Flag {
+        cli.StringFlag{"deployment,dep", "<bson_id>", "The id for the deployment for tailing logs"},
+      },
       Action: func(c *cli.Context) {
+        requireArguments("deployments:logs", c, []string{"deployment"}, []string{})
         fmt.Println("Pending")
       },
     },
@@ -122,19 +108,10 @@ func main() {
       Name:      "deployments:oplog",
       Usage:     "tail oplog",
       Flags:     []cli.Flag {
-        cli.StringFlag{"deployment,dep", "", "<deployment id>"},
+        cli.StringFlag{"deployment,dep", "<bson_id>", "The id for the deployment to tail an oplog"},
       },
       Action: func(c *cli.Context) {
-        err := false
-        if c.String("deployment") == "" {
-          err = true
-          fmt.Println("--deployment is required")
-        }
-
-        if err {
-          os.Exit(1)
-        }
-
+        requireArguments("deployments:oplog", c, []string{"deployment"}, []string{})
         controllers.DeploymentOplog(c.String("deployment"))
       },
     },
@@ -149,10 +126,11 @@ func main() {
       Name:      "users",
       Usage:     "list users on a database",
       Flags:     []cli.Flag {
-        cli.StringFlag { "deployment,dep", "<deployment id>", ""},
-        cli.StringFlag { "database,db", "<database name>", ""},
+        cli.StringFlag { "deployment,dep", "<bson_id>", "The deployment id the database is on"},
+        cli.StringFlag { "database,db", "<string>", "The specific database to list users"},
       },
       Action: func(c *cli.Context) {
+        requireArguments("users", c, []string{"deployment", "database"}, []string{})
         controllers.DatabaseUsers(c.String("deployment"), c.String("database"))
       },
     },
@@ -160,11 +138,12 @@ func main() {
       Name:      "users:create",
       Usage:     "add user to a database",
       Flags:     []cli.Flag {
-        cli.StringFlag { "deployment,dep", "<deployment id>", ""},
-        cli.StringFlag { "database,db", "<database name>", ""},
-        cli.StringFlag { "username,u", "<user>", ""},
+        cli.StringFlag { "deployment,dep", "<bson_id>", "The deployment id the database is on"},
+        cli.StringFlag { "database,db", "<string>", "The database name to create the user on"},
+        cli.StringFlag { "username,u", "<string>", "The new user to create"},
       },
       Action: func(c *cli.Context) {
+        requireArguments("users:create", c, []string{"deployment", "database", "username"}, []string{})
         controllers.DatabaseCreateUser(c.String("deployment"), c.String("database"), c.String("username"))
       },
     },
@@ -172,11 +151,12 @@ func main() {
       Name:      "users:remove",
       Usage:     "remove user from database",
       Flags:     []cli.Flag {
-        cli.StringFlag { "deployment,dep", "<deployment id>", ""},
-        cli.StringFlag { "database,db", "<database name>", ""},
-        cli.StringFlag { "username,u", "<user>", ""},
+        cli.StringFlag { "deployment,dep", "<bson_id>", "The deployment id the database is on"},
+        cli.StringFlag { "database,db", "<string>", "The database name to remove the user from"},
+        cli.StringFlag { "username,u", "<string>", "The user to remove from the deployment"},
       },
       Action: func(c *cli.Context) {
+        requireArguments("users:remove", c, []string{"deployment", "database", "username"}, []string{})
         controllers.DatabaseRemoveUser(c.String("deployment"), c.String("database"), c.String("username"))
       },
     },
