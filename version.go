b16def5caddc3f1bd3ef5a2e08ab80203606db0b
Chris Winslett <chris@mongohq.com>
2014-05-05 14:12:40 -0700

    Dynamic versioning on circle builds

--- version.go
+++ version.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-  "os"
-)
-
 func Version() string {
-  var suffix string
-  if os.Getenv("CIRCLE_BUILD_NUM") != "" {
-    suffix = os.Getenv("CIRCLE_BUILD_NUM")
-  } else {
-    suffix = "snowflake"
-  }
-
-  return "0.0.1-" + suffix
+  return "0.0.1-snowflake"
 }
